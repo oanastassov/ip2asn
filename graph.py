e5383b19c21cb2e69354650ce066d22f65f3e865
oanastassov <75079980+oanastassov@users.noreply.github.com>
2021-07-26 15:11:25 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,9 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime, timedelta
+plt.style.use('seaborn')
 
 with open('datenumasn.txt') as d:
     lines = d.readlines()
-    x = [line.split()[0] for line in lines]
-    y = [line.split()[1] for line in lines]
-plt.plot(x,y)
+    count = 0
+    dates = []
+    y = []
+    for line in lines:
+        date = line.split()[0].split("-")
+        year = int(date[0])
+        month = int(date[1])
+        day = int(date[2]) 
+        dates.append(datetime(year = year, month = month, day = day))
+        y.append(int(line.split()[1]))
+        count = count + 1
+plt.plot_date(dates,y,linestyle = "solid", marker = "None")
+plt.yscale("log")
 plt.show()
+
