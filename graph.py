5d87088247ffca158f658d7daf1d87a835cda214
oanastassov <75079980+oanastassov@users.noreply.github.com>
2021-07-26 15:29:44 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -16,7 +16,16 @@ with open('datenumasn.txt') as d:
         dates.append(datetime(year = year, month = month, day = day))
         y.append(int(line.split()[1]))
         count = count + 1
-plt.plot_date(dates,y,linestyle = "solid", marker = "None")
+
+plot1 = plt.subplot2grid((3, 3), (0, 0), rowspan=2, colspan=2)
+plot2 = plt.subplot2grid((3, 3), (2, 0), rowspan=2, colspan=2)
+
+plot1.plot(dates,y,linestyle = "solid", marker = "None")
+plot1.set_title('# ASNs over Time')
+
+plot2.plot(dates,y,linestyle = "solid", marker = "None")
+plot2.set_title('Logarithmic Scale of # ASNs over Time')
+
+plt.tight_layout()
 plt.yscale("log")
 plt.show()
-
